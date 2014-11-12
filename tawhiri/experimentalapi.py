96238483312de99164c92a674541052f468dfb94
Rich Wareham <rjw57@cam.ac.uk>
2014-11-12 17:00:04 +0000

    apiv2: create KML/KMZ directly

--- tawhiri/experimentalapi.py
+++ tawhiri/experimentalapi.py
@@ -20,12 +20,11 @@ Provide an experimental HTTP API for Tawhiri as a Flask Blueprint.
 """
 from collections import namedtuple
 from contextlib import contextmanager
-import json
 import random
 
 from flask import Blueprint, jsonify, request, current_app
 import numpy as np
-from osgeo import ogr, gdal
+from osgeo import ogr, osr, gdal
 import pyproj
 from werkzeug.exceptions import BadRequest, NotFound
 
@@ -57,37 +56,62 @@ def predict():
         ),
     ))
 
-# Mapping between prediction output formats and OGR drivers.
+# Mapping between prediction output formats and OGR drivers, extensions and
+# media types.
 PREDICT_FORMAT_MAP = {
-    'geojson': ('GeoJSON', 'json'), # NB. GeoJSON is natively emitted.
-    'kml': ('LibKML', 'kml'),
+    'kmz': ('LibKML', 'kmz', 'application/vnd.google-earth.kmz'),
+    'kml': ('LibKML', 'kml', 'application/vnd.google-earth.kml+xml'),
 }
 
-@api.route('/predict.<format>', methods=['POST'])
-def predict_reformat(format):
-    if format not in PREDICT_FORMAT_MAP:
+WGS84_OGR_SRS = osr.SpatialReference(osr.SRS_WKT_WGS84)
+assert WGS84_OGR_SRS is not None
+
+WGS84_PROJECTION = pyproj.Proj(init='epsg:4326')
+GEOCENT_PROJECTION = pyproj.Proj(proj='geocent', datum='WGS84', units='m')
+
+@api.route('/predict.<ext>', methods=['POST'])
+def predict_reformat(ext):
+    # Which OGR driver should we use for this?
+    try:
+        driver_name, extension, media_type = PREDICT_FORMAT_MAP[ext]
+    except TypeError as e:
+        # If we're running in debug mode, raise the original exception
+        # otherwise, treat as if this endpoint doesn't exit.
+        if current_app.debug:
+            raise e
         raise NotFound()
 
     # Parse request body
     prediction_spec = parse_prediction_spec_request()
 
-    # Modal tracks
-    modal_tracks = run_modal_prediction(prediction_spec)
-
     # Run predictions
+    modal_tracks = run_modal_prediction(prediction_spec)
     results = run_predictions(prediction_spec)
 
     # Compute some stats
     l_mean, l_cov = compute_run_statistics(results)
 
-    geojson_fc = runs_to_geojson(modal_tracks, l_mean, l_cov)
+    # Transform into output SRS
+    modal_tracks_wgs84 = list(geocentric_to_wgs84(t) for t in modal_tracks)
 
-    if format == 'geojson':
-        # We're done
-        return jsonify(geojson_fc)
+    # Create output file and OGR data source
+    with vsi_tempfile('/vsimem/output', extension) as out_fn:
+        drv = ogr.GetDriverByName(driver_name)
+        assert drv is not None
+        dst_ds = drv.CreateDataSource(out_fn)
+        assert dst_ds is not None
 
-    # Otherwise, convert via OGR
-    return ogr_convert_geojson(geojson_fc, *PREDICT_FORMAT_MAP[format])
+        # Create output features
+        append_tracks_layer(dst_ds, modal_tracks_wgs84)
+        append_2d_covariance_layer(dst_ds, l_mean, l_cov)
+
+        # Release dataset
+        dst_ds.Destroy()
+
+        # Read out body
+        body_data = vsi_read(out_fn)
+
+    return (body_data, 200, {'Content-Type': media_type})
 
 LaunchSpec = namedtuple('LaunchSpec', ['lng', 'lat', 'alt', 'when'])
 SimpleAltitudeProfile = namedtuple(
@@ -100,6 +124,21 @@ PredictionSpec = namedtuple(
 )
 
 ### FORMATTING OUTPUT ###
+
+def vsi_read(path):
+    """Use GDAL's VSI routines to read the contents of the file at path."""
+    # Read out body
+    with vsi_open(path, 'rb') as file_handle:
+        assert file_handle is not None
+
+        # How much data is there?
+        gdal.VSIFSeekL(file_handle, 0, 2)
+        total_size = gdal.VSIFTellL(file_handle)
+        gdal.VSIFSeekL(file_handle, 0, 0)
+
+        file_data = gdal.VSIFReadL(1, total_size, file_handle)
+        assert file_data is not None
+    return file_data
 
 @contextmanager
 def vsi_open(path, mode):
@@ -121,72 +160,77 @@ def vsi_tempfile(prefix, extension):
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     temp_id = ''.join(list(random.choice(alpha) for _ in range(6)))
     name = '{0}{1}.{2}'.format(prefix, temp_id, extension)
+
+    # Create, close and unlink the file to make sure it doesn't exist.
     hn = gdal.VSIFOpenL(name, 'w')
     gdal.VSIFCloseL(hn)
+    gdal.Unlink(name)
+
     yield name
-    rv = gdal.Unlink(name)
-    assert rv == 0
 
-def ogr_convert_geojson(src, driver_name, extension):
-    in_tmp = vsi_tempfile('/vsimem/input', 'geojson')
-    out_tmp = vsi_tempfile('/vsimem/output', extension)
+    # Now unlink the file if it exists
+    gdal.Unlink(name)
 
-    with in_tmp as in_path, out_tmp as out_path:
-        gdal.FileFromMemBuffer(in_path, json.dumps(src))
-        src_ds = ogr.Open(in_path)
-        assert src_ds is not None
+def append_tracks_layer(ds, legs, layer_name='legs'):
+    """Create a layer in ds with features representing legs as a LineString.
 
-        drv = ogr.GetDriverByName(driver_name)
-        assert drv is not None
+    Legs must be a sequence of Nx4 arrays with rows being time, longitude,
+    latitude and altitude relative to the WGS84 datum.
 
-        dst_ds = drv.CreateDataSource(out_path)
-        assert dst_ds is not None
+    """
+    # Create layer
+    layer = ds.CreateLayer(layer_name, geom_type=ogr.wkbLineString)
+    assert layer is not None
 
-        for l_idx in range(src_ds.GetLayerCount()):
-            layer = src_ds.GetLayerByIndex(l_idx)
-            dst_ds.CopyLayer(layer, layer.GetName())
+    # Create fields
+    layer.CreateField(ogr.FieldDefn('altitudeMode', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('times', ogr.OFTRealList))
 
-        dst_ds.SyncToDisk()
+    for leg in legs:
+        # Create leg geometry
+        leg_geom = ogr.Geometry(ogr.wkbLineString)
+        assert leg_geom is not None
+        leg_geom.AssignSpatialReference(WGS84_OGR_SRS)
 
-        with vsi_open(out_path, 'rb') as dst_handle:
-            assert dst_handle is not None
+        for _, lng, lat, alt in leg:
+            leg_geom.AddPoint(lng, lat, alt)
 
-            # How much data is there
-            gdal.VSIFSeekL(dst_handle, 0, 2)
-            total_size = gdal.VSIFTellL(dst_handle)
-            gdal.VSIFSeekL(dst_handle, 0, 0)
+        # Create leg feature
+        feature = ogr.Feature(layer.GetLayerDefn())
+        assert feature is not None
+        feature.SetGeometry(leg_geom)
+        feature.SetStyleString('PEN(c:#00FF00)')
+        feature.SetField('altitudeMode', 'absolute')
+        feature.SetFieldDoubleList(
+            feature.GetFieldIndex('times'), leg[:, 0].tolist()
+        )
+        layer.CreateFeature(feature)
 
-            dst_data = gdal.VSIFReadL(1, total_size, dst_handle)
-            assert dst_data is not None
+    return feature
 
-    return dst_data
+def append_2d_covariance_layer(ds, mean, covariance, layer_name='uncertainty'):
+    """Create a layer in ds with features representing the 4x4 covariance
+    matrix cov centres on mean. The covariance and mean dimensions are assumed
+    to be ordered time, x, y, z. (I.e. in geocrentric co-ordinates.).
 
-def runs_to_geojson(modal_tracks, l_mean, l_cov):
-    modal_tracks_wgs84 = list(geocentric_to_wgs84(t) for t in modal_tracks)
+    The covariance axes with maximum uncertainty are chosen and projected onto
+    the ground. 1, 2, 3 and 4 sigma ellipses are then drawn.
 
-    # List to hold output features
-    features = []
+    """
+    # Create layer
+    layer = ds.CreateLayer(layer_name, geom_type=ogr.wkbPolygon)
+    assert layer is not None
 
-    # Create modal track feature
-    features.append(dict(
-        type='Feature',
-        geometry=dict(
-            type='MultiLineString',
-            coordinates=list(t[:, 1:].tolist() for t in modal_tracks_wgs84),
-        ),
-        properties=dict(
-            times=list(t[:, 0].tolist() for t in modal_tracks_wgs84),
-            OGR_STYLE='PEN(c:#00FF00)',
-            altitudeMode='absolute',
-        ),
-    ))
+    # Create fields
+    layer.CreateField(ogr.FieldDefn('altitudeMode', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('drawOrder', ogr.OFTInteger))
 
-    # Compute landing covariance 1 sigma vectors (ignoring time)
-    lambdas, sigma_vs = np.linalg.eig(l_cov[1:, 1:])
+    # Compute covariance 1 sigma vectors (ignoring time)
+    lambdas, sigma_vs = np.linalg.eig(covariance[1:, 1:])
     for col_idx in range(3):
         sigma_vs[:, col_idx] *= np.sqrt(np.maximum(0, lambdas[col_idx]))
 
-    # Columns of sigma_vs are lat, lng, alt triples pointing along principal
+    # Columns of sigma_vs are x, y, z triples pointing along principal
     # axes of ellipse of uncertainty. Choose those with *largest* uncertainty.
     sigma_sq_mags = np.sum(sigma_vs ** 2, axis=0)
     ellipse_axes = sigma_vs[:, np.argsort(sigma_sq_mags)[-2:]]
@@ -199,39 +243,46 @@ def runs_to_geojson(modal_tracks, l_mean, l_cov):
 
         thetas = np.linspace(0, 2*np.pi, n_thetas)
         sins, coss = np.sin(thetas), np.cos(thetas)
-        landing_poly_coords = np.repeat(
-            l_mean[1:].reshape((1, -1)), n_thetas, axis=0)
-        landing_poly_coords += np.dot(
+        poly_coords = np.repeat(mean[1:].reshape((1, -1)), n_thetas, axis=0)
+        poly_coords += np.dot(
             sins.reshape((-1, 1)),
             sigma * ellipse_axes[:, 0].reshape((1, -1))
         )
-        landing_poly_coords += np.dot(
+        poly_coords += np.dot(
             coss.reshape((-1, 1)),
             sigma * ellipse_axes[:, 1].reshape((1, -1))
         )
 
         # Convert polygon co-ordinates to WGS84 lng/lats. Notice that we ignore
         # altitude.
-        lp_coords_wgs84 = np.zeros((n_thetas, 2))
-        lp_coords_wgs84[:, 0], lp_coords_wgs84[:, 1], _ = pyproj.transform(
+        coords_wgs84 = np.zeros((n_thetas, 2))
+        coords_wgs84[:, 0], coords_wgs84[:, 1], _ = pyproj.transform(
             GEOCENT_PROJECTION, WGS84_PROJECTION,
-            landing_poly_coords[:, 0], landing_poly_coords[:, 1],
-            landing_poly_coords[:, 2]
+            poly_coords[:, 0], poly_coords[:, 1], poly_coords[:, 2]
         )
 
-        features.append(dict(
-            type='Feature',
-            geometry=dict(
-                type='Polygon',
-                coordinates=[lp_coords_wgs84.tolist()],
-            ),
-            properties=dict(
-                drawOrder=len(sigma_fills) - sigma_idx,
-                OGR_STYLE='BRUSH(fc:{0});PEN(c:#000000)'.format(fill),
-            )
-        ))
+        # Create geometry with appropriate coords
+        geom = ogr.Geometry(ogr.wkbPolygon)
+        assert geom is not None
+        geom.AssignSpatialReference(WGS84_OGR_SRS)
 
-    return dict(type='FeatureCollection', features=features)
+        ring_geom = ogr.Geometry(ogr.wkbLinearRing)
+        assert ring_geom is not None
+        ring_geom.AssignSpatialReference(WGS84_OGR_SRS)
+        for lng, lat in coords_wgs84:
+            ring_geom.AddPoint(lng, lat)
+        geom.AddGeometry(ring_geom)
+
+        # Create feature
+        feature = ogr.Feature(layer.GetLayerDefn())
+        assert feature is not None
+        feature.SetGeometry(geom)
+        feature.SetStyleString('PEN(c:#000000);BRUSH(fc:{0})'.format(fill))
+        feature.SetField('altitudeMode', 'absolute')
+        feature.SetField('drawOrder', len(sigma_fills) - sigma_idx)
+        layer.CreateFeature(feature)
+
+    return feature
 
 ### RUNNING THE PREDICTOR ###
 
@@ -243,9 +294,6 @@ def ruaumoko_ds():
         ruaumoko_ds.once = ElevationDataset(ds_loc)
 
     return ruaumoko_ds.once
-
-WGS84_PROJECTION = pyproj.Proj(init='epsg:4326')
-GEOCENT_PROJECTION = pyproj.Proj(proj='geocent', datum='WGS84', units='m')
 
 def solver_to_geocentric(observations):
     """Take a Nx4 array of time, latitude, longitude, altitude observations and

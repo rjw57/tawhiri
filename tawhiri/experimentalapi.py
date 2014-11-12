fb84e85231c7c6fc7f9b5ce81d5d2f04072baa2e
Rich Wareham <rjw57@cam.ac.uk>
2014-11-12 00:09:48 +0000

    api2: experimental GeoJSON output

--- tawhiri/experimentalapi.py
+++ tawhiri/experimentalapi.py
@@ -37,25 +37,88 @@ def index():
 
 @api.route('/predict', methods=['POST'])
 def predict():
-    # Parse request body as JSON. Note that get_json() will return None if the
-    # request body is empty.
-    json = request.get_json()
-    if json is None:
-        raise BadRequest()
-
-    # Parse JSON body into a prediction spec.
-    try:
-        prediction_spec = prediction_spec_from_json(json)
-    except (IndexError, ValueError, KeyError) as e:
-        if current_app.debug:
-            raise e
-        else:
-            raise BadRequest()
+    # Parse request body
+    prediction_spec = parse_prediction_spec_request()
 
     # Run predictions
-    means, ellps = run_predictions(prediction_spec)
+    results = run_predictions(prediction_spec)
 
-    return jsonify(dict(means=means.tolist(), sigmavecs=ellps.tolist()))
+    # Compute some stats
+    mean_track, landing_mean, landing_cov = compute_run_statistics(results)
+
+    return jsonify(dict(
+        mean=mean_track.tolist(),
+        landing=dict(
+            mean=landing_mean.tolist(),
+            cov=landing_cov.tolist(),
+        ),
+    ))
+
+@api.route('/predict.geojson', methods=['POST'])
+def predict_geojson():
+    # Parse request body
+    prediction_spec = parse_prediction_spec_request()
+
+    # Run predictions
+    results = run_predictions(prediction_spec)
+
+    # Compute some stats
+    mean_track, l_mean, l_cov = compute_run_statistics(results)
+
+    # List to hold output features
+    features = []
+
+    # Create mean track feature
+    features.append(dict(
+        type='Feature',
+        geometry=dict(
+            type='LineString', coordinates=mean_track[:, 1:].tolist()
+        ),
+        properties=dict(
+            times=mean_track[:, 0].tolist(),
+            OGR_STYLE='PEN(c:#00FF00)',
+            altitudeMode='absolute',
+        ),
+    ))
+
+    # Compute landing covariance 1 sigma vectors (ignoring time)
+    lambdas, sigma_vs = np.linalg.eig(l_cov[1:,1:])
+    for col_idx in range(3):
+        sigma_vs[:, col_idx] *= np.sqrt(np.maximum(0, lambdas[col_idx]))
+
+    # Columns of sigma_vs are lat, lng, alt triples pointing along principal
+    # axes of ellipse of uncertainty. Choose those with *smallest* altitudes.
+    ellipse_axes = sigma_vs[:, np.argsort(sigma_vs[2, :])[:2]]
+
+    # Generate ellipse
+    thetas = np.linspace(0, 2*np.pi, 32)
+    sins, coss = np.sin(thetas), np.cos(thetas)
+    landing_poly_coords = np.repeat(
+        l_mean[1:].reshape((1, -1)), thetas.shape[0], axis=0)
+    landing_poly_coords += np.dot(
+        sins.reshape((-1, 1)),
+        ellipse_axes[:, 0].reshape((1, -1))
+    )
+    landing_poly_coords += np.dot(
+        coss.reshape((-1, 1)),
+        ellipse_axes[:, 1].reshape((1, -1))
+    )
+    features.append(dict(
+        type='Feature',
+        geometry=dict(
+            type='Polygon',
+            coordinates=[landing_poly_coords.tolist()],
+        ),
+        properties=dict(
+            altitudeMode='absolute',
+            OGR_STYLE='BRUSH(fc:#FF000080);PEN(c:#FFFFFF00)',
+        )
+    ))
+
+    return jsonify(dict(
+        type='FeatureCollection',
+        features=features
+    ))
 
 LaunchSpec = namedtuple('LaunchSpec', ['lng', 'lat', 'alt', 'when'])
 SimpleAltitudeProfile = namedtuple(
@@ -81,8 +144,11 @@ def ruaumoko_ds():
 def run_predictions(spec):
     """Sample and run predictions according to the passed PredictionSpec.
 
-    """
+    Returns a sequence of a Nx4 arrays where each row is a prediction for a
+    different time point.  Each prediction is a 4-tuple of UNIX timestamp,
+    latitude, longitude, altitude.
 
+    """
     # Find wind data location
     ds_dir = current_app.config.get(
         'WIND_DATASET_DIR', WindDataset.DEFAULT_DIRECTORY
@@ -92,7 +158,7 @@ def run_predictions(spec):
     tawhiri_ds = WindDataset.open_latest(persistent=True, directory=ds_dir)
 
     # TODO: make this Python2 friendly
-    results, max_shape = [], (0, 0)
+    results = []
     for _ in range(spec.sample_count):
         # Draw ascent profile
         ascent_rate = spec.profile.ascent_rate()
@@ -114,78 +180,55 @@ def run_predictions(spec):
         # Concatenate legs together into single Nx4 array
         data = np.vstack(solver.solve(when, lat, lng, alt, stages))
 
-        # Change time axis to be *relative* offsets
-        data[:, 0] -= data[0, 0]
-
-        max_shape = tuple(max(ms, s) for ms, s in zip(max_shape, data.shape))
-
         # Add to data list
         results.append(data)
 
-    return compute_run_stats(results, max_shape)
+    return results
 
-def cov_to_sigma_ellipse(cov):
-    """Return array whose columns represent one sigma vectors along the
-    principal axes.
+def compute_run_statistics(results):
+    """Compute statistics from a sequence of runs."""
 
-    """
-    # Compute eigenvalues and eigenvectors
-    lambdas, vs = np.linalg.eig(cov)
+    # Compute landing points, sum and sample count. When computing running sum,
+    # convert time axis to *relative* time.
+    max_len = np.max(list(r.shape[0] for r in results))
+    landing_points = np.zeros((len(results), 4))
+    sample_sum = np.zeros((max_len, 4))
+    sample_count = np.zeros((max_len,), np.int)
+    for r_idx, res in enumerate(results):
+        landing_points[r_idx, :] = res[-1, :]
+        sample_sum[:res.shape[0], :] += res
+        sample_sum[:res.shape[0], 0] -= res[0, 0]
+        sample_count[:res.shape[0]] += 1
 
-    # Compute "one sigma" vectors
-    for idx, sigma in enumerate(np.sqrt(np.maximum(0, lambdas))):
-        vs[:, idx] *= sigma
-    vs = np.ma.getdata(np.ma.fix_invalid(vs, fill_value=0))
+    # Compute mean track
+    mean_track = sample_sum.copy()
+    mean_track /= np.repeat(sample_count.reshape((-1, 1)), 4, axis=1)
 
-    # Choose most vertical (i.e. one with greatest abs z component)
-    most_vert_idx = np.argmax(np.abs(vs[2, :]))
-    vert_v = np.atleast_2d(vs[:, most_vert_idx]).T
-    if vert_v[2, 0] < 0:
-        vert_v *= -1
-    vs = np.hstack((vs[:, :most_vert_idx], vs[:, most_vert_idx+1:]))
+    # Compute mean and covariance of landing
+    landing_mean = np.mean(landing_points, axis=0)
+    landing_cov = np.cov(landing_points.T)
 
-    # Choose most north-south
-    most_ns_idx = np.argmax(np.abs(vs[1, :]))
-    ns_v = np.atleast_2d(vs[:, most_ns_idx]).T
-    if ns_v[1, 0] < 0:
-        ns_v *= -1
-    vs = np.hstack((vs[:, :most_ns_idx], vs[:, most_ns_idx+1:]))
-
-    assert vs.shape[1] == 1
-    if vs[0, 0] < 0:
-        vs *= -1
-
-    # Use this order
-    vs = np.hstack((vert_v, ns_v, vs))
-
-    return vs
-
-def compute_run_stats(results, max_shape):
-    # Form combined array of timesteps x obs x samples
-    combined_runs = np.zeros(max_shape + (len(results),))
-    combined_runs[:] = np.nan
-    for idx, r in enumerate(results):
-        combined_runs[:r.shape[0], :, idx] = r
-    assert combined_runs.shape[1] == 4
-
-    # Convert combined_runs into a masked array
-    combined_runs = np.ma.masked_invalid(combined_runs)
-
-    # Compute ensemble mean
-    means = np.ma.mean(combined_runs, axis=-1)
-
-    # Compute one sigma ellipses. Note that since we're dealing with relative
-    # time, we expect the covariance of the time dimension to be negligible.
-    ellps = list(
-        cov_to_sigma_ellipse(np.ma.getdata(np.ma.cov(row[1:, :])))
-        for row in combined_runs
-    )
-
-    # Output is means (Nx4), ellps (Nx4x4). Each "row" of ellps being an array
-    # whose rows are the one-sigma vectors.
-    return np.ma.getdata(means), np.dstack(ellps).T
+    return mean_track, landing_mean, landing_cov
 
 ### PARSING OF SPEC FROM JSON REQUESTS ###
+
+def parse_prediction_spec_request():
+    # Parse request body as JSON. Note that get_json() will return None if the
+    # request body is empty.
+    json = request.get_json()
+    if json is None:
+        raise BadRequest()
+
+    # Parse JSON body into a prediction spec.
+    try:
+        prediction_spec = prediction_spec_from_json(json)
+    except (IndexError, ValueError, KeyError) as e:
+        if current_app.debug:
+            raise e
+        else:
+            raise BadRequest()
+
+    return prediction_spec
 
 def prediction_spec_from_json(json):
     """Construct a new PredictionSpec from a dictionary representing a
